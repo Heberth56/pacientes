12be26adc9a4e79560f60975b9a9acb8f855abb1
dedstinguerdsds <dedstinguerdsds@gmail.com>
2024-11-04 01:17:06 -0400

    django cors

--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:8000",
+    "https://pacientes-dev.onrender.com"
 ]
 
 CORS_ALLOW_METHODS = [
